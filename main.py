9b571abc110ac5cc6320c8b4776dd9e75a7850db
shangliy <kudoysl@gmail.com>
2017-01-04 20:47:41 -0800

    static almost

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
@@ -17,48 +18,6 @@ def weighted_img(img, initial_img, α=0.8, β=1., λ=0.):
     NOTE: initial_img and img must be the same shape!
     """
     return cv2.addWeighted(initial_img, α, img, β, λ)
-
-def draw_poly_lines(img, lines, color=[0, 0, 255], thickness=10):
-    points_left_x =np.array([])
-    points_left_y =np.array([])
-    left_num = 0
-    points_right_x =np.array([])
-    points_right_y =np.array([])
-    right_num = 0
-    for line in lines:
-        for x1,y1,x2,y2 in line:
-            if ((y2-y1)/(x2-x1)<0):
-                left_num = left_num + 2;
-                points_left_x = np.append(points_left_x, [x1,x2])
-                points_left_y = np.append(points_left_y, [y1,y2])
-            if ((y2-y1)/(x2-x1)>0):
-                right_num = right_num + 2;
-                points_right_x = np.append(points_right_x, [x1,x2])
-                points_right_y = np.append(points_right_y, [y1,y2])
-            print (str(x1)+' '+str(y1)+' '+str(x2)+' '+str(y2))
-
-    print (points_left_y.shape)
-    print (points_left_x.shape)
-    k_left = np.poly1d(np.polyfit(points_left_y, points_left_x, 3))
-    k_right = np.poly1d(np.polyfit(points_right_y, points_right_x, 3))
-
-    points_left = np.array([])
-    points_right = np.array([])
-    num = 0
-    for y_tem in range(350,img.shape[0]):
-        num = num + 1
-        x_left = k_left(y_tem)
-        x_right = k_right(y_tem)
-        points_left = np.append(points_left, [x_left,y_tem])
-        points_right = np.append(points_right, [x_right,y_tem])
-    #print (points_left.shape)
-    #print (points_right.shape)
-    points_left = points_left.reshape(num,2)
-    points_right = points_right.reshape(num,2)
-    #print (points_left)
-    #sys.exit()
-    cv2.polylines(img, np.int32([points_left]),False, thickness=thickness, color=color)
-    cv2.polylines(img, np.int32([points_right]),False, thickness= thickness, color=color)
 
 def draw_lines(img, lines, color=[0, 0, 255], thickness=10):
     """
@@ -123,16 +82,16 @@ def draw_lines(img, lines, color=[0, 0, 255], thickness=10):
 
     y1_left_fin = 450
     x1_left_fin = x2_left - (y2_left-y1_left_fin)/k_left
-    y2_left_fin = img.shape[0]
+    y2_left_fin = img.shape[0]-100
     x2_left_fin = x2_left - (y2_left-y2_left_fin)/k_left
 
     y1_right_fin = 450
     x1_right_fin = x2_right - (y2_right-y1_right_fin)/k_right
-    y2_right_fin = img.shape[0]
+    y2_right_fin = img.shape[0]-100
     x2_right_fin = x2_right - (y2_right-y2_right_fin)/k_right
 
-    cv2.line(img, (int(x1_left_fin), int(y1_left_fin)), (int(x2_left_fin), int(y2_left_fin)), color, thickness)
-    cv2.line(img, (int(x1_right_fin), int(y1_right_fin)), (int(x2_right_fin), int(y2_right_fin)), color, thickness)
+    return([x1_left_fin,y1_left_fin],[x2_left_fin,y2_left_fin],[x1_right_fin,y1_right_fin]
+           ,[x2_right_fin,y2_right_fin])
 
 def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap):
     """
@@ -143,9 +102,9 @@ def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap):
     lines = cv2.HoughLinesP(img, rho, theta, threshold, np.array([]), minLineLength=min_line_len, maxLineGap=max_line_gap)
     line_img = np.zeros(img.shape, dtype=np.uint8)
     line_colorimge = np.dstack((line_img,line_img,line_img))
-    draw_lines(line_colorimge, lines)
+    a,b,c,d = draw_lines(line_colorimge, lines)
 
-    return line_colorimge
+    return a,b,c,d
 
 def region_of_interest(img, vertices):
     """
@@ -203,7 +162,7 @@ def mag_thresh(img, sobel_kernel=3, mag_thresh=(0, 255)):
     # Calculate the gradient magnitude
     gradmag = np.sqrt(sobelx**2 + sobely**2)
     # Rescale to 8 bit
-    scale_factor = np.max(gradmag)/255
+    scale_factor = np.max(gradmag)/255.
     gradmag = (gradmag/scale_factor).astype(np.uint8)
     # Create a binary image of ones where threshold is met, zeros otherwise
     binary_output = np.zeros_like(gradmag)
@@ -248,12 +207,12 @@ def hls_select(img, thresh=(0, 255)):
 # Choose a Sobel kernel size
 ksize = 3 # Choose a larger odd number to smooth gradient measurements
 # Read in an image
-image = mpimg.imread('signs_vehicles_xygrad.png')
+image = mpimg.imread('test3.jpg')
 
 # Apply each of the thresholding functions
-gradx = abs_sobel_thresh(image, orient='x', sobel_kernel=ksize, thresh=(30, 100))
-grady = abs_sobel_thresh(image, orient='y', sobel_kernel=ksize, thresh=(30, 100))
-mag_binary = mag_thresh(image, sobel_kernel=ksize, mag_thresh=(50, 100))
+gradx = abs_sobel_thresh(image, orient='x', sobel_kernel=ksize, thresh=(20, 100))
+grady = abs_sobel_thresh(image, orient='y', sobel_kernel=ksize, thresh=(20, 100))
+mag_binary = mag_thresh(image, sobel_kernel=ksize, mag_thresh=(40, 100))
 dir_binary = dir_threshold(image, sobel_kernel=ksize, thresh=(np.pi/6, np.pi/4))
 s_binary = hls_select(image, thresh=(90, 255))
 
@@ -268,19 +227,130 @@ masked_edges = region_of_interest(combined,vertices)
 # Make a blank the same size as our image to draw on
 rho = 2 # distance resolution in pixels of the Hough grid
 theta = np.pi/180 # angular resolution in radians of the Hough grid
-threshold = 20     # minimum number of votes (intersections in Hough grid cell)
-min_line_length = 20 #minimum number of pixels making up a line
-max_line_gap = 10    # maximum gap in pixels between connectable line segments
+threshold = 30     # minimum number of votes (intersections in Hough grid cell)
+min_line_length = 30 #minimum number of pixels making up a line
+max_line_gap = 20    # maximum gap in pixels between connectable line segments
 
-lines_image = hough_lines(masked_edges, rho, theta, threshold, min_line_length, max_line_gap)
+a,b,c,d = hough_lines(masked_edges, rho, theta, threshold, min_line_length, max_line_gap)
 #mat_array = cv2.fromarray(masked_edges)
-color_edges = np.dstack((masked_edges,masked_edges,masked_edges))
+src =  np.float32([a,c,d,b])
+offset = 100 # offset for dst points
+dst = np.float32([[offset, 0], [imshape[0]-offset, 0],
+                                     [imshape[0]-offset, imshape[1]],
+                                     [offset, imshape[1]]])
+
+print(src)
+print(dst)
+M = cv2.getPerspectiveTransform(src, dst)
+Minv = cv2.getPerspectiveTransform(dst,src)
+warped = cv2.warpPerspective(combined,M,imshape)
+#color_edges = np.dstack((masked_edges,masked_edges,masked_edges))
 #result = weighted_img(color_edges,lines_image,0.8, 1., 0)
+
+
+histogram = np.sum(warped[warped.shape[0]/2:,:], axis=0)
+
+
+win_left = np.argmax(histogram[:int(warped.shape[1]/2)])
+win_right = int(warped.shape[1]/2)+np.argmax(histogram[int(warped.shape[1]/2):])
+print (win_left)
+print (win_right)
+
+leftx = np.zeros((1001,))
+leftx[0] = win_left
+
+rightx = np.zeros((1001,))
+rightx[0] = win_right
+
+for yl in range(1,1000):
+    yvals = warped.shape[0] - yl
+
+
+
+    if np.max(np.sum(warped[yvals-10:yvals,win_left-15:win_left+15], axis=0)) < 1:
+
+        leftx[yl] = win_left
+        win_left = win_left
+    else:
+        lx = win_left-15 + np.argmax(np.sum(warped[yvals-10:yvals,win_left-15:win_left+15], axis=0))
+        leftx[yl] = lx
+        win_left = lx
+
+
+    if np.max(np.sum(warped[yvals-10:yvals,win_right-15:win_right+15], axis=0)) < 1:
+        rightx[yl] = win_right
+        win_right = win_right
+    else:
+        rx = win_right-15 + np.argmax(np.sum(warped[yvals-10:yvals,win_right-15:win_right+15], axis=0))
+        rightx[yl] = rx
+        win_right = rx
+
+
+
+leftx = leftx[::-1]
+rightx = rightx[::-1]  # Reverse to match top-to-bottom in y
+
+yvals = np.linspace(0, 1000, num=1001)*1.  # to cover same y-range as image
+left_fit = np.polyfit(yvals, leftx, 2)
+left_fitx = left_fit[0]*yvals**2 + left_fit[1]*yvals + left_fit[2]
+right_fit = np.polyfit(yvals, rightx, 2)
+right_fitx = right_fit[0]*yvals**2 + right_fit[1]*yvals + right_fit[2]
+
+# Plot up the fake data
+plt.plot(leftx, yvals, 'o', color='red')
+plt.plot(rightx, yvals, 'o', color='blue')
+plt.xlim(0, 800)
+plt.ylim(0, 1100)
+plt.plot(left_fitx, yvals, color='green', linewidth=3)
+plt.plot(right_fitx, yvals, color='green', linewidth=3)
+plt.gca().invert_yaxis() # to visualize as we do the images
+plt.show()
+
+y_eval = np.max(yvals)
+left_curverad = ((1 + (2*left_fit[0]*y_eval + left_fit[1])**2)**1.5) \
+                             /np.absolute(2*left_fit[0])
+right_curverad = ((1 + (2*right_fit[0]*y_eval + right_fit[1])**2)**1.5) \
+                                /np.absolute(2*right_fit[0])
+print(left_curverad, right_curverad)
+
+# Create an image to draw the lines on
+warp_zero = np.zeros_like(warped).astype(np.uint8)
+color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
+
+# Recast the x and y points into usable format for cv2.fillPoly()
+pts_left = np.array([np.transpose(np.vstack([left_fitx, yvals]))])
+pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, yvals])))])
+pts = np.hstack((pts_left, pts_right))
+
+# Draw the lane onto the warped blank image
+cv2.fillPoly(color_warp, np.int_([pts]), (0,255, 0))
+# Warp the blank back to original image space using inverse perspective matrix (Minv)
+newwarp = cv2.warpPerspective(color_warp, Minv, (image.shape[1], image.shape[0]))
+# Combine the result with the original image
+result = cv2.addWeighted(image, 1, newwarp, 0.3, 0)
+plt.imshow(result)
+
+
+ym_per_pix = 30/1000 # meters per pixel in y dimension
+xm_per_pix = 3.7/600 # meteres per pixel in x dimension
+
+left_fit_cr = np.polyfit(yvals*ym_per_pix, leftx*xm_per_pix, 2)
+right_fit_cr = np.polyfit(yvals*ym_per_pix, rightx*xm_per_pix, 2)
+left_curverad = ((1 + (2*left_fit_cr[0]*y_eval + left_fit_cr[1])**2)**1.5) \
+                             /np.absolute(2*left_fit_cr[0])
+right_curverad = ((1 + (2*right_fit_cr[0]*y_eval + right_fit_cr[1])**2)**1.5) \
+                                /np.absolute(2*right_fit_cr[0])
+# Now our radius of curvature is in meters
+print(left_curverad, 'm', right_curverad, 'm')
+# Example values: 3380.7 m    3189.3 m
+
+
+
 f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
 f.tight_layout()
 ax1.imshow(masked_edges)
 ax1.set_title('Original Image', fontsize=50)
-ax2.imshow(lines_image, cmap='gray')
+ax2.imshow(warped, cmap='gray')
 ax2.set_title('Combined image', fontsize=50)
 plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
 plt.show()
